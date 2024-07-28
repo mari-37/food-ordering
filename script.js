ef5f24fffc21060c28b334cddadcc2815154c541
mari-37 <marimuthu03032007@gmail.com>
2024-07-28 16:04:37 +0530

    feat:nested html by react.createElement

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-const heading = React.createElement("h1",{},"WELCOME TO REACT WORLD");
+const parent = React.createElement("div", { id:parent }, [
+    React.createElement("h1",{},"h1 tag from child 1"),
+    React.createElement("h2",{},"h2 tag from child 1"),
+    React.createElement("h3",{},"h3 tag from child 1"),
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
