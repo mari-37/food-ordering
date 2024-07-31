ff99eeb3c92079bc1488d07b65c0f931309d33c7
mari-37 <marimuthu03032007@gmail.com>
2024-07-31 23:12:42 +0530

    feat : useState hook basics

--- readme.md
+++ readme.md
@@ -123,6 +123,25 @@
 # optional chaining ?.
      -The optional chaining (?.) operator accesses an object's property or calls a function. If the object accessed or function called using this operator is undefined or null, the expression short circuits and evaluates to undefined instead of throwing an error.
 
+# 2 types of import
+    normal import    -- export default component ;     --   import component from "path" ;
+    name import      -- export const component = ;     --   import {component} from "path" ;
+
+
+# hooks
+
+
+# local state variable
+
+
+# usestate()
+
+
+# react fibre
+
+
+# diff algorithm
+
 
 
 
--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,13 +1,23 @@
 import React from "react";
 import resList from "../utils/mockData" ;
 import ResCard from "./ResCard";
+import { useState } from "react";
 
 
 const Body = () => {
+const [listOfRes,SetlistOfRes] = useState(resList) ;
+
     return(
         <div className="body">
+            <button className="filter-btn" onClick={ ()=>{
+              const  filteredRes = listOfRes.filter((res) => res.info.avgRating >4  ) ;
+                 SetlistOfRes(filteredRes)
+                }}>
+              TOP RATED RESTAURANT
+            </button>
+
            <div className="res-container">
-            {resList.map((res) => (
+            {listOfRes.map((res) => (
              <ResCard key={res?.info?.id} resData={res} />
             ))}           
            </div>   
